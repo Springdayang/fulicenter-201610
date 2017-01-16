98e1ad5a0428f6eaf65603045f207c61bdfd6790
dayang <xyadayang@126.com>
2017-01-16 10:05:11 +0800

    添加排序图片

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/CategoryListActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/CategoryListActivity.java
@@ -1,5 +1,6 @@
 package cn.ucai.fulicenter.controller.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +13,9 @@ import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.I;
 import cn.ucai.fulicenter.controller.fragment.NewsGoodsFragment;
 
+/**
+ * 分类列表
+ */
 public class CategoryListActivity extends AppCompatActivity {
     NewsGoodsFragment mNewGoodsFragment;
     @BindView(R.id.btn_sort_price)
@@ -34,21 +38,30 @@ public class CategoryListActivity extends AppCompatActivity {
     @OnClick({R.id.btn_sort_price, R.id.btn_sort_addtime})
     public void onClick(View view) {
         int sortBy =I.SORT_BY_ADDTIME_ASC;
+        Drawable right;
         switch (view.getId()) {
             case R.id.btn_sort_price:
                 if(priceAsc){
                     mNewGoodsFragment.sortGoods(I.SORT_BY_PRICE_ASC);
+                    right=getResources().getDrawable(R.mipmap.arrow_order_up);
                 }else{
                     mNewGoodsFragment.sortGoods(I.SORT_BY_PRICE_DESC);
+                    right=getResources().getDrawable(R.mipmap.arrow_order_down);
                 }
+                right.setBounds(0,0,right.getIntrinsicWidth(),right.getIntrinsicHeight());
+                btnSortPrice.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,right,null);
                 priceAsc=!priceAsc;
                 break;
             case R.id.btn_sort_addtime:
                 if(addTimeAsc){
                     mNewGoodsFragment.sortGoods(I.SORT_BY_ADDTIME_ASC);
+                    right=getResources().getDrawable(R.mipmap.arrow_order_up);
                 }else{
                     mNewGoodsFragment.sortGoods(I.SORT_BY_ADDTIME_DESC);
+                    right=getResources().getDrawable(R.mipmap.arrow_order_down);
                 }
+                right.setBounds(0,0,right.getIntrinsicWidth(),right.getIntrinsicHeight());
+                btnSortAddtime.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,right,null);
                 addTimeAsc=!addTimeAsc;
                 break;
         }
